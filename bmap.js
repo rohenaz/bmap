b7ee0cb80af32d5b302be08b2496ef515d82cdd9
Satchmo <rohenaz@gmail.com>
2019-06-02 09:51:59 -0400

    discover encoding, default to binary

--- bmap.js
+++ bmap.js
@@ -1,4 +1,13 @@
 const bmap = {}
+
+Map.prototype.getKey = function (searchValue) {
+  for (let [key, value] of this.entries()) {
+    if (value === searchValue)
+      return key
+  }
+  return null
+}
+
 // Takes a bitdb formatted op_return transaction
 bmap.TransformTx = (tx) => {
   if (!tx || !tx.hasOwnProperty('in') || !tx.hasOwnProperty('out')) {
@@ -8,19 +17,14 @@ bmap.TransformTx = (tx) => {
   let protocolMap = new Map()
   protocolMap.set('B','19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut')
   protocolMap.set('MAP','1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5')
+  protocolMap.set('META', 'META')
   protocolMap.set('AIP','15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva')
-
-  let protocolSchema = {
-    '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut': 'B',
-    '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5': 'MAP',
-    '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva': 'AIP'
-  }
-
+      
   let querySchema = {
     'B': [
-      { 'content': 'string' },
+      { 'content': ['string', 'binary'] },
       { 'content-type': 'string' },
-      { 'encoding': 'string' },
+      { 'encoding': 'string' }, // we try to use this to determine content character encoding. If encoding is not a valid character encoding, we assume it is binary
       { 'filename': 'string' }
     ],
     'MAP': [
@@ -30,6 +34,7 @@ bmap.TransformTx = (tx) => {
         { 'val': 'string' }
       ]
     ],
+    'METANET': [],
     'AIP': [
       { 'algorithm': 'string' },
       { 'address': 'string' },
@@ -43,10 +48,18 @@ bmap.TransformTx = (tx) => {
   // This will become our nicely formatted response object
   let dataObj = {}
 
+  // offsets record the position of each protocol
+  let offsets = new Map()
+
   // We always know what the first protocol is, it's always in s1
-  let protocolName = protocolSchema[tx.out.filter(tx => { return tx && tx.b0.op === 106 })[0].s1]
-  // Flag for handling pipes
-  let relativeIndex = 0
+  let prefix = tx.out.filter(tx => { return tx && tx.b0.op === 106 })[0].s1
+
+  // If s1 does not contain a protocol prefix, there's nothing to do
+  if (!protocolMap.getKey(prefix)) {
+    throw new Error('Unrecognized transaction', tx)
+  }
+
+  let protocolName = protocolMap.getKey(prefix)
 
   // Loop over the tx keys (in, out, tx, blk ...)
   for (let key of Object.keys(tx)) {
@@ -55,132 +68,121 @@ bmap.TransformTx = (tx) => {
       // There can be only one
       let opReturnOutput = tx[key][0]
 
-      // used to loop over repeating schema patterns
+      // FIRST, we separate the string, key, and binary values
+      let valueMaps = {
+        'binary': new Map(),
+        'string': new Map(),
+        'hex': new Map()
+      }
+
+      let otherVals = {}
+      let indexCount = 0
       let roundIndex = 0
 
-      // walk pushdatas
       for (let pushdataKey in opReturnOutput) {
-        // if the 's' key push is unavailable, assume the 'l' variant exists
-        pushdataKey = opReturnOutput.hasOwnProperty(pushdataKey) ? pushdataKey : opReturnOutput.hasOwnProperty('l' + pushdataKey) ? 'l' + pushdataKey : null
-        if (!pushdataKey) {
-          // missing large data fields (ex. chronos bitsocket)
-          pushdataKey = 'lb' + relativeIndex
-          pushdataKey = 'ls' + relativeIndex
+        // console.log('key', pushdataKey)
+        // Get the TXO index number by itself (strip letters)
+        let num = parseInt(pushdataKey.replace(/[A-Za-z]/g,''))
+        if (num >= 0) {
+          if (pushdataKey.startsWith('s') || pushdataKey.startsWith('ls')) {
+            valueMaps.string.set(num, opReturnOutput[pushdataKey])
+          } else if(pushdataKey.startsWith('b') || pushdataKey.startsWith('lb')) {
+            valueMaps.binary.set(num, opReturnOutput[pushdataKey])
+          } else if(pushdataKey.startsWith('h') || pushdataKey.startsWith('lh')) {
+            valueMaps.hex.set(num, opReturnOutput[pushdataKey])
+          }
+          if (num > indexCount) {
+            indexCount = num
+          }
+        } else {
+          otherVals[pushdataKey] = opReturnOutput[pushdataKey]
         }
-        let pushdata = opReturnOutput[pushdataKey]
+      }
 
-        // TODO - only do this if the field in question should be a string
-        if ((!pushdataKey.startsWith('s') && !pushdataKey.startsWith('ls')) || pushdataKey === 'str') {
-          // skip the non string values
-          continue
-        }
+      // Loop for pushdata count and find appropriate value
+      let relativeIndex = 0
+      for (let x = 0; x < indexCount; x++) {
 
-        // If its an op, we dont need it now...
-        if (pushdata.hasOwnProperty('op')) {
-          continue
-        }
-
-        // If pushdata is the protocol name, skip it
-        if (Object.keys(protocolSchema).some((address) => { return address === pushdata }) && relativeIndex === 0) {
-          protocolName = protocolSchema[pushdata]
-          continue
-        }
-
-        // If its the protocol prefix, we dont need it now...
-        if (!relativeIndex) {
-          // new Protocol
+        if (relativeIndex === 0 && protocolMap.getKey(valueMaps.string.get(x + 1))) {
+          protocolName = protocolMap.getKey(valueMaps.string.get(x + 1))
           dataObj[protocolName] = []
+          offsets.set(protocolName, x+1)
+          continue
         }
 
-        // if the value is a pipe, set the name again and continue without pushing
-        if (pushdata === '|') {
+        // Detect UNIX pipeline
+        if (valueMaps.string.get(x+1) === '|') {
+          // console.log('========================= End', protocolName)
           relativeIndex = 0
           continue
         }
 
-        let schemaKey
-        // Check for an array for this query schema key (repeating pattern)
-        let schemaField = querySchema[protocolName][relativeIndex]
-        let obj = {}
-        if (schemaField instanceof Array) {
-          // loop through the schema as we add values
-          roundIndex = roundIndex % schemaField.length
-          let thekey = Object.keys(schemaField[roundIndex++])[0]
-          obj[thekey] = pushdata
-          dataObj[protocolName].push(obj)
-          continue
-        } else {
-          // get the key,value pair from this query schema
-          if (schemaField) {
-            schemaKey = Object.keys(schemaField)[0]
-            obj[schemaKey] = pushdata
-          }
+        //console.log('Value is', valueMaps[encoding].get(x), '(',encoding,')')
+        let encoding
+        if (relativeIndex !== 0) {
+          // get the schema object, or array of objects in case of repeating fields
+          let schemaField = querySchema[protocolName][relativeIndex-1]
+          if (!schemaField) { throw new Error('Failed to find schema field for ', protocolName) }
 
-          dataObj[protocolName].push(obj)
+          let obj = {}
+
+          if (schemaField instanceof Array) {
+            // loop through the schema as we add values
+            roundIndex = roundIndex % schemaField.length
+            let thekey = Object.keys(schemaField[roundIndex++])[0]
+            encoding = Object.values(schemaField[roundIndex++])[0]
+            obj[thekey] = valueMaps[encoding].get(x)
+
+            dataObj[protocolName].push(obj)
+            continue
+          } else {
+            // get the key, value pair from this query schema
+          
+            let schemaKey = Object.keys(schemaField)[0]
+            let schemaEncoding = Object.values(schemaField)[0]
+
+            // B has many encoding possibilities for content, look in index 2 relative to the protocol schema
+            if (schemaEncoding instanceof Array) {                
+              // if encoding field if not included in content array assume its binary
+              let encodingLocation = 's' + (offsets.get(protocolName) + 2 + relativeIndex)
+              encoding = schemaEncoding.includes(opReturnOutput[encodingLocation]) ? opReturnOutput[encodingLocation] : 'binary'
+              console.log('key', schemaKey, 'options', schemaEncoding, 'encoding', encoding)
+            } else {
+              encoding = schemaEncoding
+            }
+            
+            obj[schemaKey] = valueMaps[encoding].get(x)
+
+            // console.log('push for', protocolName, 'to field', schemaField, 'on', dataObj[protocolName], 'full', dataObj)
+            dataObj[protocolName].push(obj)
+            relativeIndex++
+          }
+        } else {
           relativeIndex++
         }
       }
-    }
-    dataObj[key] = tx[key]
-  }
 
-  // TRANSFORM
-  let newMap = {}
-  let newB = {}
-  let newAIP = {}
-  let keyTemp
-  let self = dataObj
-  if (self.hasOwnProperty('MAP')) {
-    for (let kv of self.MAP) {
-      let key = Object.keys(kv)[0]
-      if (key === 'key') {
-        keyTemp = Object.values(kv)[0]
-      } else if (key === 'val') {
-        newMap[keyTemp] = Object.values(kv)[0]
-      }
-    }
-    self.MAP = newMap
-  }
-
-  if (self.hasOwnProperty('B')) {
-    for (let kv of self.B) {
-      let key = Object.keys(kv)[0]
-      newB[key] = Object.values(kv)[0]
-    }
-
-    // Detect & swap binary encoding for B files
-    if (newB.hasOwnProperty('encoding') && (newB['encoding'] === 'binary' || newB['encoding'] === 'gzip') && self.out.some(out => { return out && out.s1 === protocolMap.get('B') && (out.s4 === 'binary' || out.s4 === 'gzip') })) {
-      newB['content'] = self.out.filter(out => { return out && out.s1 === protocolMap.get('B') }).map((out) => { return out.lb2 })[0]
-    }
-
-    self.B = newB
-  }
-
-  if (self.hasOwnProperty('AIP')) {
-    for (let kv of self.AIP) {
-      let key = Object.keys(kv)[0]
-      newAIP[key] = Object.values(kv)[0]
-    }
-
-    // Detect and swap signature
-    if (newAIP.hasOwnProperty('signature') && newAIP.signature.length > 0) {
-
-      // find where the bad signature lives, and replace 's' with 'b'
-      let opOuts = self.out.filter((out) => { return out && out.hasOwnProperty('b0') && out.b0.hasOwnProperty('op') && out.b0.op === 106 })
-      for (let [key, val] of Object.entries(opOuts[0])) {
-        if (val === newAIP.signature) {
-          // replace signature with binary
-          newAIP.signature = opOuts[0][key.replace('s','b')]
-          break
+      // TRANSFORM MAP from {key: "keyname", val: "myval"} to {keyname: 'myval'}
+      let keyTemp
+      let newMap = {}
+      if (dataObj.hasOwnProperty('MAP')) {
+        let i = 0
+        for (let kv of dataObj.MAP) {
+          let key = Object.keys(kv)[0]
+          let value = Object.values(kv)[0]
+          if (key === 'cmd') { newMap.cmd = value; continue }
+          if (i % 2 === 0) {
+            keyTemp = value
+          } else {
+            newMap[keyTemp] = value
+          }
+          i++
         }
+        dataObj.MAP = newMap
       }
+
+      return dataObj
     }
-
-    self.AIP = newAIP
   }
-
-  // Now my object is ready
-  return self
 }
-
 export default bmap
