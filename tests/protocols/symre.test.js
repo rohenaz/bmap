17b9325a4d91a96daf0033276ccf706234f1fea0
Luke <rohenaz@gmail.com>
2021-01-12 21:47:18 -0500

    cleaning up demo page wip

--- tests/protocols/symre.test.js
+++ tests/protocols/symre.test.js
@@ -1,4 +1,6 @@
-import { describe, expect, beforeEach, afterEach, test, } from '@jest/globals';
+import {
+  describe, expect, test
+} from '@jest/globals';
 import { SYMRE } from '../../src/protocols/symre';
 import symreTransactions from '../data/symre-haip-transactions.json';
 
@@ -21,8 +23,8 @@ describe('symre', () => {
 
   test('parse tx', () => {
     const dataObj = {};
-    const cell = symreTransactions[0].out[0].tape[1].cell;
-    const tape = symreTransactions[0].out[0].tape;
+    const { cell } = symreTransactions[0].out[0].tape[1];
+    const { tape } = symreTransactions[0].out[0];
     const tx = symreTransactions[0];
 
     SYMRE.handler(dataObj, cell, tape, tx);
@@ -30,3 +32,8 @@ describe('symre', () => {
     expect(dataObj.SYMRE.url).toEqual('https://medium.com/@Stas33496115/bitcoin-script-engineering-part-ii-ba8095f093c0');
   });
 });
+
+// TODO: Test old Symre format (or just remove this if its unimportant)
+// symre_old: [
+//   'de6e22a88a7739325793941c53eab6c39b8f817e15f4e305ea6a084040f271f9',
+// ],
